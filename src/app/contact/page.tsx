2c5ba4630e6eca32059d0fc1e3fb8b248e056f3e
Harsh Yadav <hy945196@gmail.com>
2024-01-06 10:41:35 +0530

    Addded pages

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,35 @@
+import Image from "next/image";
+import styles from "./contact.module.css";
+
+
+export const metadata = {
+  title: "Contact Page",
+  description: "Contact description",
+};
+
+
 const contact = () => {
   return (
-    <div>contact</div>
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image src="/contact.png" alt="" fill className={styles.img} />
+      </div>
+      <div className={styles.formContainer}>
+        <form action="" className={styles.form}>
+          <input type="text" placeholder="Name and Surname" />
+          <input type="text" placeholder="Email Address" />
+          <input type="text" placeholder="Phone Number (Optional)" />
+          <textarea
+            name=""
+            id=""
+            cols="20"
+            rows="6"
+            placeholder="Message"
+          ></textarea>
+          <button>Send</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
